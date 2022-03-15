9dd785f5316324210424549f6753c84708ebc5d3
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-03-14 21:03:53 -0300

    create upcoming and lanches views and setup tabBarView

---
+++ SpaceX/SpaceX/Screams/LaunchesViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchesViewController.swift
+//  SpaceX
+//
+//  Created by Grazielli Berti on 14/03/22.
+//
+
+import UIKit
+
+class LaunchesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
---
+++ SpaceX/SpaceX/Screams/UpcomingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingViewController.swift
+//  SpaceX
+//
+//  Created by Grazielli Berti on 14/03/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+}
